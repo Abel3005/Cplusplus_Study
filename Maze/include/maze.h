6b9ff9152a5c7c98de5c8bf859f1e1cbed29e85a
Abel3005 <48238305+Abel3005@users.noreply.github.com>
2024-02-25 23:13:34 +0900

    Update maze.h

--- Maze/include/maze.h
+++ Maze/include/maze.h
@@ -1,1 +1,11 @@
 #pragma once
+
+#define  ROAD  0
+#define  PASS  1
+#define  BLOCK  2
+#define  NOWAY  3
+#define  MOUSE  4
+#define  START  5
+#define  END  6
+#define  WALL  7
+#define MAX_STACK_SIZE  10000
